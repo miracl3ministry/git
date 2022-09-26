4e78b945bb7516ada2a3c281f3ac3a250c1830cc
miracl3ministry <dimasovic123@gmail.com>
2022-09-26 22:02:30 +0500

    initial

---
+++ lab1/lab1.py
@@ -0,0 +1,17 @@
+# Список
+listOfCountries = ['US','DE','NL','PL','RU','UA']
+# Кортеж
+canAccept = ('DE','NL',"PL")
+# Словарь
+lead = {
+    'name': 'ivan',
+    'second_name' : 'ivanov',
+    'phone' : '79999999999',
+    'email' : 'email@mail.ru',
+    'utm' : [],
+    'host' : '127.0.0.1',
+    'ip' : '192.168.0.11',
+    'country' : 'RU',
+}
+# Множество
+hosts = set(['site1.ru', 'site2.ru', 'site3.ru', 'advertising.ru'])
